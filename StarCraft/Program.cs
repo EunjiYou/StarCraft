58ccd6d96fdb95ba49246de0db3d704570dd7b2c
김태현 <cplkimth@naver.com>
2018-07-17 14:27:04 +0900

    .

---
+++ StarCraft/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarCraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Marine m1 = new Marine();
+
+            // m1이 스팀팩 사용
+            m1.HP = m1.HP - 5;
+            m1.Speed = m1.Speed + 1;
+
+            Marine m2 = new Marine();
+
+            // m2가 스팀팩 사용
+            m2.HP = m2.HP - 5;
+            m2.Speed = m2.Speed + 1;
+
+            // m1이 스팀팩 사용
+            m1.HP = m1.HP - 5;
+            m1.Speed = m1.Speed + 1;
+        }
+    }
+}
