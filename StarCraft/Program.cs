cd543ef380ac1d276ef1cfdb6754f12f14cada25
김태현 <cplkimth@naver.com>
2018-07-17 15:50:57 +0900

    파이어뱃 생성

--- StarCraft/Program.cs
+++ StarCraft/Program.cs
@@ -10,21 +10,26 @@ namespace StarCraft
         static void Main(string[] args)
         {
             Marine m1 = new Marine();
-
             // m1이 스팀팩 사용
-            int hp1 = UseSteamPack(m1, 2);
+            int hp1 = Marine_UseSteamPack(m1, 2);
             
             Marine m2 = new Marine();
-
             // m2가 스팀팩 사용
-            int hp2 = UseSteamPack(m2, 1);
-
+            int hp2 = Marine_UseSteamPack(m2, 1);
             Heal(m1, 30);
+            Marine_GetDamaged(m2, 3);
 
-            GetDamaged(m2, 3);
+            Firebat f1 = new Firebat();
+            Firebat_UseSteamPack(f1, 1);
+            Firebat_GetDamaged(f1, 3);
         }
 
-        private static void GetDamaged(Marine m, int enemyAttack)
+        private static void Firebat_GetDamaged(Firebat f, int enemyAttack)
+        {
+            f.HP -= (enemyAttack * 3);
+        }
+
+        private static void Marine_GetDamaged(Marine m, int enemyAttack)
         {
             m.HP -= (enemyAttack * 2);
         }
@@ -40,20 +45,37 @@ namespace StarCraft
         //    return m.HP;
         //}
 
-        static int UseSteamPack(Marine m, int count)
+        static int Marine_UseSteamPack(Marine m, int count)
         {
             for (int i = 0; i < count; i++)
             {
-                UseSteamPackCore(m);
+                Marine_UseSteamPackCore(m);
             }
 
             return m.HP;
         }
 
-        static void UseSteamPackCore(Marine m)
+        static int Firebat_UseSteamPack(Firebat f, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Firebat_UseSteamPackCore(f);
+            }
+
+            return f.HP;
+        }
+
+        static void Marine_UseSteamPackCore(Marine m)
         {
             m.HP = m.HP - 5;
             m.Speed = m.Speed + 1;
+        }
+        // Ctrl + D (duplicate)
+
+        static void Firebat_UseSteamPackCore(Firebat f)
+        {
+            f.HP = f.HP - 5;
+            f.Speed = f.Speed + 1;
         }
 
         static void Heal(Marine m, int hpToAdd)
